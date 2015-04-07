70e56f1f6ac141e3f821960c717faf9c0868f48b
Elvira <kolesofortuni@mail.ru>
2015-04-08 01:27:24 +0400

    Splash Screen Animation

--- app/src/main/java/ru/tp/buy_places/activities/SplashScreenActivity.java
+++ app/src/main/java/ru/tp/buy_places/activities/SplashScreenActivity.java
@@ -1,9 +1,11 @@
 package ru.tp.buy_places.activities;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.ImageView;
 
 import ru.tp.buy_places.R;
 
@@ -12,6 +14,17 @@ public class SplashScreenActivity extends ActionBarActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        final ImageView splashImageView =
+                (ImageView) findViewById(R.id.SplashImageView);
+        splashImageView.setBackgroundResource(R.drawable.splash);
+        final AnimationDrawable frameAnimation =
+                (AnimationDrawable)splashImageView.getBackground();
+        splashImageView.post(new Runnable(){
+            @Override
+            public void run() {
+                frameAnimation.start();
+            }
+        });
         final int SPLASH_SCREEN_DURATION = getResources().getInteger(R.integer.splash_screen_duration);
         new Handler().postDelayed(new Runnable() {
             @Override
