b27ea8f964eba5b88947ad0f52081c5ed998d3ac
krygin.ia <krygin.ia@gmail.com>
2015-04-08 00:09:30 +0300

    Deals

--- app/src/main/java/ru/tp/buy_places/content_provider/BuyPlacesContract.java
+++ app/src/main/java/ru/tp/buy_places/content_provider/BuyPlacesContract.java
@@ -1,13 +1,18 @@
 package ru.tp.buy_places.content_provider;
 
+import android.net.Uri;
+
 /**
  * Created by Ivan on 07.04.2015.
  */
 public final class BuyPlacesContract {
+    public static final String AUTHORITY = "ru.tp.buy_places.provider";
+    public static final Uri AUTHORITY_URI = Uri.parse("content://" + AUTHORITY);
+
+
     private BuyPlacesContract () {
 
     }
-
 
 
 
