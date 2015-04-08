43724147bb683d8911a1737bf96f0524d5f3db2b
krygin.ia <krygin.ia@gmail.com>
2015-04-09 00:17:38 +0300

    Map

--- app/src/main/java/ru/tp/buy_places/fragments/map/MapFragment.java
+++ app/src/main/java/ru/tp/buy_places/fragments/map/MapFragment.java
@@ -1,21 +1,21 @@
 package ru.tp.buy_places.fragments.map;
 
-
-import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.MapView;
 
 import ru.tp.buy_places.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MapFragment extends SupportMapFragment {
+public class MapFragment extends Fragment {
 
+    private MapView mapView;
 
     public MapFragment() {
         // Required empty public constructor
@@ -25,8 +25,34 @@ public class MapFragment extends SupportMapFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        super.onCreateView(inflater, container, savedInstanceState);
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_map, container, false);
+        mapView = (MapView) rootView.findViewById(R.id.map_view);
+        mapView.onCreate(savedInstanceState);
+        return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onStart();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 }
