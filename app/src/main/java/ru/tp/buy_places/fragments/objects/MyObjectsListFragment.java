3640560935cf1a08f44ebb55734c454ff79258bb
Elvira <kolesofortuni@mail.ru>
2015-04-26 10:44:17 +0400

    activities obj, user

--- app/src/main/java/ru/tp/buy_places/fragments/objects/MyObjectsListFragment.java
+++ app/src/main/java/ru/tp/buy_places/fragments/objects/MyObjectsListFragment.java
@@ -1,6 +1,7 @@
 package ru.tp.buy_places.fragments.objects;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -21,6 +22,7 @@ import java.util.HashMap;
 
 import ru.tp.buy_places.R;
 import ru.tp.buy_places.ServiceHelper;
+import ru.tp.buy_places.activities.ObjectActivity;
 
 import static ru.tp.buy_places.content_provider.BuyPlacesContract.Places;
 
@@ -105,11 +107,8 @@ public class MyObjectsListFragment extends Fragment implements LoaderManager.Loa
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                ObjectFragment objectFragment = new ObjectFragment();
-                getFragmentManager().
-                        beginTransaction().
-                        replace(R.id.content,objectFragment).
-                        commit();
+                Intent intent = new Intent(getActivity(), ObjectActivity.class);
+                startActivity(intent);
             }
         });
         return mListView;
